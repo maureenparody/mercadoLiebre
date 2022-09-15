6181749b8ad2dbb19b6fb16e2cc36b0551dffc62
maureenparody <maureenparody@gmail.com>
2022-09-15 19:41:24 -0300

    help

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/mostrar_mensaje', function (req, res) {
   res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.use(express.static(path.resolve(__dirname, './public')));
+// app.use(express.static(path.resolve(__dirname, './public')));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Servidor corriendo");
