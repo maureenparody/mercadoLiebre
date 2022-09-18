48c0a3f496d88a63b38256ec13a7b66408e29d16
maureenparody <maureenparody@gmail.com>
2022-09-18 15:36:42 -0300

    solucionado

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const path = require('path');
 
 const app = express();
 
-app.get('/mostrar_mensaje', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/register', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 app.use(express.static(path.resolve(__dirname, './public')));
